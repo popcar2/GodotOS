1f373631694d72aba759775ef81fbc10a1dbca57
popcar2 <ahmoudy2000@gmail.com>
2023-10-22 20:08:47 +0300

    Add desktop folder highlight

--- Scenes/Desktop/desktop_folder.gd
+++ Scenes/Desktop/desktop_folder.gd
@@ -1,5 +1,22 @@
 extends Control
 
-func _gui_input(event):
-	if event is InputEventMouse:
-		print("AHHH!!")
+func _ready():
+	$Highlight.self_modulate.a = 0
+
+func _on_mouse_entered():
+	show_highlight()
+
+func _on_mouse_exited():
+	hide_highlight()
+
+# ------
+
+func show_highlight():
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property($Highlight, "self_modulate:a", 1, 0.25).from(0.1)
+
+func hide_highlight():
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property($Highlight, "self_modulate:a", 0, 0.25)
