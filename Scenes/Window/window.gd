495d5b78ddf91106fe5406d63fb16fb7c91d42e4
popcar2 <ahmoudy2000@gmail.com>
2023-10-23 19:12:34 +0300

    Select and deselect windows

--- Scenes/Window/window.gd
+++ Scenes/Window/window.gd
@@ -7,11 +7,17 @@ var mouse_start_drag_position: Vector2
 
 var is_being_deleted: bool
 var is_minimized: bool
+var is_selected: bool = true
 
 signal minimized(is_minimized: bool)
 signal deleted()
 
 func _ready():
+	get_parent().move_child(self, 3)
+	for window in get_tree().get_nodes_in_group("window"):
+		if window == self: continue
+		window.deselect_window()
+	
 	modulate.a = 0
 	var tween: Tween = create_tween()
 	tween.set_trans(Tween.TRANS_CUBIC)
@@ -21,9 +27,14 @@ func _process(_delta):
 	if is_dragging:
 		global_position = start_drag_position + (get_global_mouse_position() - mouse_start_drag_position)
 
+func _gui_input(event: InputEvent):
+	if event is InputEventMouseButton and event.button_index == 1 and event.is_pressed():
+		select_window()
+
 func _on_top_bar_gui_input(event):
 	if event is InputEventMouseButton and event.button_index == 1:
 		if event.is_pressed():
+			select_window()
 			is_dragging = true
 			start_drag_position = global_position
 			mouse_start_drag_position = get_global_mouse_position()
@@ -63,6 +74,8 @@ func show_window():
 	if !is_minimized:
 		return
 	
+	get_parent().move_child(self, 3)
+	
 	is_minimized = false
 	minimized.emit(is_minimized)
 	
@@ -72,3 +85,26 @@ func show_window():
 	tween.set_parallel(true)
 	tween.tween_property(self, "position:y", position.y - 20, 0.25)
 	tween.tween_property(self, "modulate:a", 1, 0.25)
+
+func select_window():
+	if is_selected:
+		return
+	
+	is_selected = true
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "modulate:a", 1, 0.1)
+	get_parent().move_child(self, 3)
+	
+	for window in get_tree().get_nodes_in_group("window"):
+		if window == self: continue
+		window.deselect_window()
+
+func deselect_window():
+	if !is_selected:
+		return
+	
+	is_selected = false
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "modulate:a", 0.75, 0.25)
