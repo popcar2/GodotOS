22f8ad39480886aef3f500e1d7687d21d082ce6e
popcar2 <ahmoudy2000@gmail.com>
2023-10-25 21:36:42 +0300

    Finish window benchmark test

--- Scenes/Misc/Benchmark Tester/benchmark_test.gd
+++ Scenes/Misc/Benchmark Tester/benchmark_test.gd
@@ -3,9 +3,10 @@ extends Node
 var windows: int
 
 func _physics_process(_delta):
-	window_spawner()
+	if windows < 1500:
+		window_spawner()
+	$RichTextLabel.text = "Windows: %d\nFPS: %d" % [windows, Performance.get_monitor(Performance.TIME_FPS)]
 
 func window_spawner():
 	get_tree().get_first_node_in_group("folder").spawn_window()
 	windows += 1
-	$RichTextLabel.text = "Windows: %d\nFPS: %d" % [windows, Performance.get_monitor(Performance.TIME_FPS)]
