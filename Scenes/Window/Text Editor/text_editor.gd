9a115fa91d8fcd5e651c578e2f4f33c4e1ac2f38
popcar2 <ahmoudy2000@gmail.com>
2023-10-26 14:49:58 +0300

    WIP text editor saving

--- Scenes/Window/Text Editor/text_editor.gd
+++ Scenes/Window/Text Editor/text_editor.gd
@@ -1,7 +1,17 @@
 extends TextEdit
 
 var file: FileAccess
+var text_edited: bool
 
 func populate_text(file_path: String):
 	file = FileAccess.open("user://files/%s" % file_path, FileAccess.READ_WRITE)
 	text = file.get_as_text()
+
+func _on_text_changed():
+	if text_edited:
+		return
+	
+	text_edited = true
+	$"../../Top Bar/Title Text".text += '*' 
+
+#TODO save with ctrl+s
