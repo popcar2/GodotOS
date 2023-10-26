e646eba1d1131bd4a9bb5405a46f5e7788181ba6
popcar2 <ahmoudy2000@gmail.com>
2023-10-26 22:42:38 +0300

    Add saved notification

--- Scenes/Window/Text Editor/text_editor.gd
+++ Scenes/Window/Text Editor/text_editor.gd
@@ -28,5 +28,11 @@ func save_file():
 	
 	$"../../Top Bar/Title Text".text = $"../../Top Bar/Title Text".text.trim_suffix('*')
 	text_edited = false
+	
+	var saved_notification: Panel = load("res://Scenes/Window/Text Editor/saved_notification.tscn").instantiate()
+	saved_notification.global_position.y = global_position.y + size.y - saved_notification.size.y
+	saved_notification.global_position.x = global_position.x + size.x - saved_notification.size.x
+	get_tree().current_scene.add_child(saved_notification)
+	#TODO make this a parent child?
 
 #TODO add warning when someone exits without saving
