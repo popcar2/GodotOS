b9959a0e2d5a5d5d48846b8db09bf8c064f1d0ed
popcar2 <ahmoudy2000@gmail.com>
2023-10-29 15:43:43 +0200

    Add boot splash

---
+++ Scenes/Main/Boot Splash/boot_splash.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+var multiplier: float = 1.0
+var start_screen_size: Vector2
+
+func _ready():
+	start_screen_size = DisplayServer.window_get_size()
+	var tween: Tween = create_tween()
+	tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_EXPO)
+	tween.set_parallel(true)
+	tween.tween_property($Logo, "scale", Vector2(5, 5), 2)
+	tween.tween_property($CanvasGroup/Logo, "scale", Vector2(5, 5), 2)
+	tween.tween_property($CanvasGroup/Background, "scale", $CanvasGroup/Background.scale * 2, 2)
+	
+	tween.tween_property($Logo, "self_modulate:a", 0, 1.5)
+	
+	await get_tree().create_timer(3).timeout
+	queue_free()
+
+func _physics_process(_delta):
+	#WARNING be sure to change this if default resolution changes
+	scale = DisplayServer.window_get_size() as Vector2 / start_screen_size
+	#TODO keep aspect ratio?
