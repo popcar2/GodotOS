0be756411c2482257bc693cbabff025caf4f807e
popcar2 <ahmoudy2000@gmail.com>
2023-11-06 20:12:48 +0200

    Add window resizing while maintaining aspect ratio (by holding shift)

--- Scenes/Window/resize_drag_spot.gd
+++ Scenes/Window/resize_drag_spot.gd
@@ -16,4 +16,10 @@ func _gui_input(event):
 
 func _physics_process(_delta):
 	if is_dragging:
-		get_parent().size = start_size + get_global_mouse_position() - mouse_start_drag_position
+		# TODO optimize this a bit?
+		if Input.is_key_pressed(KEY_SHIFT):
+			var aspect_ratio: float = start_size.x / (start_size.y - 30)
+			get_parent().size.x = start_size.x + (get_global_mouse_position().x - mouse_start_drag_position.x) * aspect_ratio
+			get_parent().size.y = start_size.y + get_global_mouse_position().x - mouse_start_drag_position.x
+		else:
+			get_parent().size = start_size + get_global_mouse_position() - mouse_start_drag_position
