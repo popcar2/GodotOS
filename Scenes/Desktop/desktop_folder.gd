8a748e410e90b4ec8b0d87aff27ff2b8cfec24d7
popcar2 <ahmoudy2000@gmail.com>
2023-11-10 20:31:21 +0200

    Refactor folder title edit

--- Scenes/Desktop/desktop_folder.gd
+++ Scenes/Desktop/desktop_folder.gd
@@ -26,7 +26,7 @@ func _ready():
 		$Folder/TextureRect.modulate = IMAGE_COLOR
 
 func _input(event: InputEvent):
-	if event is InputEventMouseButton and event.is_pressed():
+	if event is InputEventMouseButton and (event.button_index == 1 or event.button_index == 2) and event.is_pressed():
 		var evLocal = make_input_local(event)
 		if !Rect2(Vector2(0,0), size).has_point(evLocal.position):
 			hide_selected_highlight()
--- Scenes/Desktop/folder_title_edit.gd
+++ Scenes/Desktop/folder_title_edit.gd
@@ -2,52 +2,66 @@ extends TextEdit
 
 func _input(event: InputEvent):
 	if event.is_action_pressed("rename") and $"../../../Selected Highlight".visible:
-		get_parent().visible = true
-		grab_focus()
-		text = %"Folder Title".text.trim_prefix("[center]").split(".")[0]
-		select_all()
+		show_rename()
 	
 	if !get_parent().visible:
 		return
 	
 	if event.is_action_pressed("ui_accept"):
-		get_parent().visible = false
-		#TODO stop file from adding periods
-		var folder: FakeFolder = $"../../.."
-		if folder.file_type != folder.file_type_enum.FOLDER:
-			var old_folder_name: String = folder.folder_name
-			folder.folder_name = "%s.%s" % [text, folder.folder_name.split('.')[-1]]
-			DirAccess.rename_absolute("user://files/%s/%s" % [folder.folder_path, old_folder_name], "user://files/%s/%s" % [folder.folder_path, folder.folder_name])
-			%"Folder Title".text = "[center]%s" % folder.folder_name
-			
-			# Reloads open windows
-			for file_manager in get_tree().get_nodes_in_group("file_manager_window"):
-				file_manager.reload_window("")
-			for text_editor in get_tree().get_nodes_in_group("text_editor_window"):
-				if text_editor.file_path == "%s/%s" % [folder.folder_path, old_folder_name]:
-					text_editor.file_path = "%s/%s" % [folder.folder_path, folder.folder_name]
-				elif text_editor.file_path == old_folder_name: # In desktop
-					text_editor.file_path = folder.folder_name 
-		
-		elif folder.file_type == folder.file_type_enum.FOLDER:
-			var old_folder_name: String = folder.folder_name
-			var old_folder_path: String = folder.folder_path
-			
-			if old_folder_path.contains("/"):
-				folder.folder_path = "%s%s" % [folder.folder_path.trim_suffix(old_folder_name), text]
-			else:
-				folder.folder_path = text
-			folder.folder_name = text
-			%"Folder Title".text = "[center]%s" % folder.folder_name
-			DirAccess.rename_absolute("user://files/%s" % old_folder_path, "user://files/%s" % folder.folder_path)
-			
-			for file_manager in get_tree().get_nodes_in_group("file_manager_window"):
-				if file_manager.file_path.begins_with(old_folder_path):
-					file_manager.file_path = file_manager.file_path.replace(old_folder_path, folder.folder_path)
-				file_manager.reload_window("")
-		
-		text = ""
+		trigger_rename()
 	
 	if event.is_action_pressed("ui_cancel"):
-		get_parent().visible = false
-		text = ""
+		cancel_rename()
+	
+	if event is InputEventMouseButton and event.is_pressed():
+		var evLocal = make_input_local(event)
+		if !Rect2(Vector2(0,0), size).has_point(evLocal.position):
+			cancel_rename()
+
+func show_rename():
+	get_parent().visible = true
+	grab_focus()
+	text = %"Folder Title".text.trim_prefix("[center]").split(".")[0]
+	select_all()
+
+func trigger_rename():
+	get_parent().visible = false
+	#TODO stop file from adding periods
+	var folder: FakeFolder = $"../../.."
+	if folder.file_type != folder.file_type_enum.FOLDER:
+		var old_folder_name: String = folder.folder_name
+		folder.folder_name = "%s.%s" % [text, folder.folder_name.split('.')[-1]]
+		DirAccess.rename_absolute("user://files/%s/%s" % [folder.folder_path, old_folder_name], "user://files/%s/%s" % [folder.folder_path, folder.folder_name])
+		%"Folder Title".text = "[center]%s" % folder.folder_name
+		
+		# Reloads open windows
+		for file_manager in get_tree().get_nodes_in_group("file_manager_window"):
+			file_manager.reload_window("")
+		for text_editor in get_tree().get_nodes_in_group("text_editor_window"):
+			if text_editor.file_path == "%s/%s" % [folder.folder_path, old_folder_name]:
+				text_editor.file_path = "%s/%s" % [folder.folder_path, folder.folder_name]
+			elif text_editor.file_path == old_folder_name: # In desktop
+				text_editor.file_path = folder.folder_name 
+	
+	elif folder.file_type == folder.file_type_enum.FOLDER:
+		var old_folder_name: String = folder.folder_name
+		var old_folder_path: String = folder.folder_path
+		
+		if old_folder_path.contains("/"):
+			folder.folder_path = "%s%s" % [folder.folder_path.trim_suffix(old_folder_name), text]
+		else:
+			folder.folder_path = text
+		folder.folder_name = text
+		%"Folder Title".text = "[center]%s" % folder.folder_name
+		DirAccess.rename_absolute("user://files/%s" % old_folder_path, "user://files/%s" % folder.folder_path)
+		
+		for file_manager in get_tree().get_nodes_in_group("file_manager_window"):
+			if file_manager.file_path.begins_with(old_folder_path):
+				file_manager.file_path = file_manager.file_path.replace(old_folder_path, folder.folder_path)
+			file_manager.reload_window("")
+	
+	text = ""
+
+func cancel_rename():
+	get_parent().visible = false
+	text = ""
