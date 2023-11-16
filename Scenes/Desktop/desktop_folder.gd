7d6b05cc157a31a23ff0eea9c21fb38ea6d46d96
popcar2 <ahmoudy2000@gmail.com>
2023-11-16 22:58:25 +0200

    Use is_mouse_over rather than evlocal in desktop folder

--- Scenes/Desktop/desktop_folder.gd
+++ Scenes/Desktop/desktop_folder.gd
@@ -27,8 +27,7 @@ func _ready():
 
 func _input(event: InputEvent):
 	if event is InputEventMouseButton and event.is_pressed():
-		var evLocal = make_input_local(event)
-		if !Rect2(Vector2(0,0), size).has_point(evLocal.position):
+		if !is_mouse_over:
 			hide_selected_highlight()
 		else:
 			show_selected_highlight()
