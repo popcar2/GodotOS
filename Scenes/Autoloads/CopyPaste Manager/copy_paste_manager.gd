cba90bed8d7181d693a494b8f9f6af6514250370
popcar2 <ahmoudy2000@gmail.com>
2023-12-02 14:56:24 +0200

    Stop infinite recursion when copying folders inside itself

--- Scenes/Autoloads/CopyPaste Manager/copy_paste_manager.gd
+++ Scenes/Autoloads/CopyPaste Manager/copy_paste_manager.gd
@@ -75,8 +75,11 @@ func paste_folder_cut(to_path: String):
 		get_tree().get_first_node_in_group("desktop_file_manager").populate_file_manager()
 
 func copy_directory_recursively(dir_path: String, to_path: String):
-	for file_name in DirAccess.get_files_at(dir_path):
-		DirAccess.copy_absolute("%s/%s" % [dir_path, file_name], "%s/%s" % [to_path, file_name])
+	if to_path.begins_with(dir_path):
+		NotificationManager.spawn_notification("ERROR: Can't copy a folder into itself!")
+		return
 	for dir_name in DirAccess.get_directories_at(dir_path):
 		DirAccess.make_dir_absolute("%s/%s" % [to_path, dir_name])
 		copy_directory_recursively("%s/%s" % [dir_path, dir_name], "%s/%s" % [to_path, dir_name])
+	for file_name in DirAccess.get_files_at(dir_path):
+		DirAccess.copy_absolute("%s/%s" % [dir_path, file_name], "%s/%s" % [to_path, file_name])
