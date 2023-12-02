0c467c69fc0b5b955df6d319647c62c41cfda897
popcar2 <ahmoudy2000@gmail.com>
2023-12-02 20:34:19 +0200

    Add power button, start menu doesn't hide when clicked

---
+++ Scenes/Taskbar/power_button.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _on_pressed():
+	# TODO add animation here
+	get_tree().quit()
--- Scenes/Taskbar/start_button.gd
+++ Scenes/Taskbar/start_button.gd
@@ -1,5 +1,7 @@
 extends MarginContainer
 
+var is_mouse_over_menu: bool
+
 func _input(event: InputEvent):
 	if event is InputEventMouseButton and event.button_index == 1 and event.is_pressed():
 		handle_mouse_click(event)
@@ -17,6 +19,9 @@ func _on_mouse_exited():
 	add_theme_constant_override("margin_top", 5)
 
 func handle_mouse_click(event: InputEvent):
+	if is_mouse_over_menu: # Mouse clicked on empty space, do nothing
+		return
+	
 	var evLocal = make_input_local(event)
 	if !Rect2(Vector2(0,0), size).has_point(evLocal.position):
 		hide_start_menu()
@@ -36,3 +41,9 @@ func hide_start_menu():
 	var tween: Tween = create_tween()
 	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
 	tween.tween_property($"../Start Menu", "position:y", 50, 0.4)
+
+func _on_start_menu_mouse_entered():
+	is_mouse_over_menu = true
+
+func _on_start_menu_mouse_exited():
+	is_mouse_over_menu = false
