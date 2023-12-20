b657ffcb7060ba812156771172a46c9acd627a5d
popcar2 <ahmoudy2000@gmail.com>
2023-12-20 20:42:26 +0200

    Add shutdown boot splash

--- Scenes/Main/Boot Splash/boot_splash.gd
+++ Scenes/Main/Boot Splash/boot_splash.gd
@@ -1,9 +1,16 @@
 extends CanvasLayer
+class_name BootSplash
 
-var start_screen_size: Vector2 = Vector2(1152, 648)
+@export var quit_animation: bool
 
 func _ready():
 	visible = true
+	if quit_animation:
+		play_quit_animation()
+	else:
+		play_animation()
+
+func play_animation():
 	var tween: Tween = create_tween()
 	tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_EXPO)
 	tween.set_parallel(true)
@@ -14,6 +21,27 @@ func _ready():
 	
 	await get_tree().create_timer(3).timeout
 	queue_free()
+
+func play_quit_animation():
+	$Logo.scale = Vector2(10, 10)
+	$Logo.self_modulate.a = 0
+	$CanvasGroup/Logo.scale = Vector2(10, 10)
+	
+	var tween: Tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	tween.set_parallel(true)
+	tween.tween_property($Logo, "scale", Vector2(1, 1), 2)
+	tween.tween_property($CanvasGroup/Logo, "scale", Vector2(1, 1), 2)
+	
+	await get_tree().create_timer(1).timeout
+	
+	var tween2: Tween = create_tween()
+	tween2.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween2.tween_property($Logo, "self_modulate:a", 1, 1.5)
+	
+	await get_tree().create_timer(2).timeout
+	get_tree().quit()
 
 func _physics_process(_delta):
 	var window_size: Vector2 = DisplayServer.window_get_size() as Vector2
--- Scenes/Taskbar/power_button.gd
+++ Scenes/Taskbar/power_button.gd
@@ -1,5 +1,8 @@
 extends Button
 
+var boot_splash_scene: PackedScene = preload("res://Scenes/Main/Boot Splash/boot_splash.tscn")
+
 func _on_pressed():
-	# TODO add animation here
-	get_tree().quit()
+	var boot_splash: BootSplash = boot_splash_scene.instantiate()
+	boot_splash.quit_animation = true
+	get_tree().root.add_child(boot_splash)
